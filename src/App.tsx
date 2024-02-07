621e88377bfd9aa213a56a428a9ec0a5fdb63781
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-02-07 17:21:36 -0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>BUG BYTE proof with dev branch</h1>
+      <h1>BUG BYTE proof with dev branch with Leonardo changes</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
