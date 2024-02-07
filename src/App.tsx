fa095286c8e97b4ea1ff693fb06d93e37e863b8e
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-02-07 17:58:51 -0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Home from "./pages/Projects";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import ProjectDetails from "./pages/ProjectDetails";
+import Projects from "./pages/Projects";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/project-details" element={<ProjectDetails />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav>
       <ul>
         <li>Bug Byte</li>
-        <li>Login</li>
+        <Link to="/login">Login</Link>
+        <button></button>
       </ul>
     </nav>
   );
