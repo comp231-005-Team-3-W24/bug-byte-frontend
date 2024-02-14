0a21c10d2be63f5e3dc9cd3d37082758656dab73
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-02-14 12:08:34 -0500

    create mock api calls

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,23 @@ export type User = {
   userName: string;
 };
 
+export type LoginDTO = {
+  email: string;
+  password: string;
+};
+
+export type RegisterDTO = {
+  name: string;
+  email: string;
+  password: string;
+  role: "tester" | "developer" | "stakeholder" | "admin";
+};
+
+export type AuthResponse = {
+  accessToken: string;
+  role: string;
+};
+
 export type Project = {
   _id: string;
   name: string;
