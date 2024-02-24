f0b66d61d75329c84ce82e622d7e4a2081697bca
amellortizt <138019361+amellortizt@users.noreply.github.com>
2024-02-24 14:27:51 -0500

    fix merge conflict problem

--- src/pages/ProjectDetails.tsx
+++ src/pages/ProjectDetails.tsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
-import { useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
-=======
 import { Link, useLocation } from "react-router-dom";
->>>>>>> amell6
 import { Project } from "../types";
 
 export default function ProjectDetails() {
   const { user } = useAuth();
   const project: Project = useLocation().state;
-<<<<<<< HEAD
 
   return (
     <div>
@@ -17,23 +12,12 @@ export default function ProjectDetails() {
       <p>Description: {project.description}</p>
       <p>Company: {project.company}</p>
       {user!.role == "tester" && (
-        <button onClick={() => alert("Open bug report form")}>
-          Create Bug Report
-        </button>
+       <button>
+       <Link to={'/bug-report'} state={ project.id }>
+       Create Bug Report
+       </Link>   
+       </button>
       )}
-=======
-  project.id
-  return (
-    <div>
-        <h1>{project.name}</h1>
-        <p>Description: {project.description}</p>
-        <p>Company: {project.company}</p>
-        <button>
-        <Link to={'/bug-report'} state={ project.id }>
-        Create Bug Report
-        </Link>   
-        </button>
->>>>>>> amell6
     </div>
   );
 }
