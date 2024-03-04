9c425b5cb9ee4236c6ce90cc87ee5fcfbc32f1e0
Yza <yasc11003@gmail.com>
2024-03-03 19:31:44 -0500

    Register Style

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,62 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { RegisterDTO } from '../types';
+import {CssVarsProvider,useColorScheme,} from "@mui/joy/styles";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel, { formLabelClasses } from "@mui/joy/FormLabel";
+import IconButton, { IconButtonProps } from "@mui/joy/IconButton";
+import Link from "@mui/joy/Link";
+import Input from "@mui/joy/Input";
+import Typography from "@mui/joy/Typography";
+import Stack from "@mui/joy/Stack";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+
+interface FormElements extends HTMLFormControlsCollection {
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+  persistent: HTMLInputElement;
+}
+interface RegisterFormElement extends HTMLFormElement {
+  readonly elements: FormElements;
+}
+
+function ColorSchemeToggle(props: IconButtonProps) {
+  const { onClick, ...other } = props;
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="outlined" color="neutral" disabled />;
+  }
+  return (
+    <IconButton
+      id="toggle-mode"
+      size="sm"
+      variant="outlined"
+      color="neutral"
+      aria-label="toggle light/dark mode"
+      {...other}
+      onClick={(event) => {
+        if (mode === "light") {
+          setMode("dark");
+        } else {
+          setMode("light");
+        }
+        onClick?.(event);
+      }}
+    >
+      {mode === "light" ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
+  );
+}
 
 const Register: React.FC = () => {
   const { register } = useAuth();
@@ -14,7 +70,7 @@ const Register: React.FC = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleRegister = async (e: React.FormEvent) => {
+  const handleRegister = async (e: React.FormEvent<RegisterFormElement>) => {
     e.preventDefault();
     try {
       await register(registerData);
@@ -30,53 +86,175 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
+    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ":root": {
+            "--Collapsed-breakpoint": "769px", 
+            "--Cover-width": "50vw", 
+            "--Form-maxWidth": "800px",
+            "--Transition-duration": "0.4s", 
+          },
+        }}
+      />
+      <Box
+        sx={(theme) => ({
+          width:
+            "clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)",
+          transition: "width var(--Transition-duration)",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          position: "relative",
+          zIndex: 1,
+          display: "flex",
+          justifyContent: "flex-end",
+          backdropFilter: "blur(12px)",
+          backgroundColor: "rgba(255 255 255 / 0.2)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundColor: "rgba(19 19 24 / 0.4)",
+          },
+        })}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100dvh",
+            width:
+              "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
+            maxWidth: "100%",
+            px: 2,
+          }}
+        >
+            <ColorSchemeToggle />
+          <Box
+            component="main"
+            sx={{
+              my: "auto",
+              py: 2,
+              pb: 5,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: 400,
+              maxWidth: "100%",
+              mx: "auto",
+              borderRadius: "sm",
+              "& form": {
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              },
+              [`& .${formLabelClasses.asterisk}`]: {
+                visibility: "hidden",
+              },
+            }}
+          >
+            <Stack gap={4} sx={{ mb: 2 }}>
+              <Stack gap={1}>
+                <Typography component="h1" level="h3">
+                  Register
+                </Typography>
+                <Typography level="body-sm">
+                  Already with Bug Byte?{" "}
+                  <Link href="/login" level="title-sm">
+                    Log In!
+                  </Link>
+                </Typography>
+              </Stack>
+            </Stack>
+            <Divider
+              sx={(theme) => ({
+                [theme.getColorSchemeSelector("light")]: {
+                  color: { xs: "#FFF", md: "text.tertiary" },
+                  "--Divider-lineColor": {
+                    xs: "#FFF",
+                    md: "var(--joy-palette-divider)",
+                  },
+                },
+              })}
+            >
+              or
+            </Divider>
+            <Stack gap={4} sx={{ mt: 2 }}>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <form onSubmit={handleRegister}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
+      <FormControl required>
+      <FormLabel>Name</FormLabel>
+          <Input
             type="text"
             id="name"
             name="name"
             value={registerData.name}
             onChange={handleChange}
           />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
+      </FormControl>
+      <FormControl required>
+      <FormLabel>Email</FormLabel>
+      <Input
             type="email"
-            id="email"
             name="email"
             value={registerData.email}
             onChange={handleChange}
           />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
+      </FormControl>
+      <FormControl required>
+      <FormLabel>Password</FormLabel>
+      <Input
             type="password"
-            id="password"
             name="password"
             value={registerData.password}
             onChange={handleChange}
           />
-        </div>
-        <div>
-          <label htmlFor="role">Role:</label>
+      </FormControl>
+      <FormControl required>
+      <FormLabel>Role: </FormLabel>
           <select id="role" name="role" value={registerData.role} onChange={handleChange}>
             <option value="tester">Tester</option>
             <option value="developer">Developer</option>
             <option value="stakeholder">Stakeholder</option>
             <option value="admin">Admin</option>
           </select>
-        </div>
-        <button type="submit">Register</button>
+      </FormControl>
+      <Stack gap={4} sx={{ mt: 2 }}>
+                  <Button type="submit" fullWidth>
+                    Register
+                  </Button>
+                  </Stack>
       </form>
-      <p>Already have an account? <a href="/login">Login</a></p>
-    </div>
+      </Stack>
+      </Box>
+      <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" textAlign="center">
+              © Bug Byte {new Date().getFullYear()}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={(theme) => ({
+          height: "100%",
+          position: "fixed",
+          right: 0,
+          top: 0,
+          bottom: 0,
+          left: "clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))",
+          transition:
+            "background-image var(--Transition-duration), left var(--Transition-duration) !important",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          backgroundColor: "background.level1",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundImage:
+            "url(https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundImage:
+              "url(https://imageio.forbes.com/blogs-images/forbestechcouncil/files/2019/01/canva-photo-editor-8-7.jpg?format=jpg&width=1440)",
+          },
+        })}
+      />
+    </CssVarsProvider>
   );
 };
 
