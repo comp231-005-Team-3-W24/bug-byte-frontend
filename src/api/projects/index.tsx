8e2e3b63b6ceb90e03f228e7bd378afccb6ea9d8
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-03-09 11:04:55 -0500

    feat: fetch projects from backend instead of mock data

--- src/api/projects/index.tsx
+++ src/api/projects/index.tsx
@@ -1,20 +1,10 @@
-import { projects } from "../../db";
 import { CreateProjectDTO, Project } from "../../types";
 import httpClient from "../httpClient";
 
 const PROJECTS_ROUTE = "/project";
 
-export function projectByUserIdRequest(id: string): Promise<Project[]> {
-  return new Promise((resolve, reject) => {
-    const result = projects.filter(({ usersId }) => usersId.includes(id));
-    resolve(result), reject("An error occurred");
-  });
-}
-
-export function getProjectsRequest(): Promise<Project[]> {
-  return new Promise((resolve, reject) => {
-    resolve(projects), reject("An error occurred");
-  });
+export async function getProjectsRequest(): Promise<Project[]> {
+  return (await httpClient.get(`${PROJECTS_ROUTE}/all`)).data;
 }
 
 export async function createProject(data: CreateProjectDTO): Promise<void> {
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { getProjectsRequest } from "../api/projects";
 import ProjectCard from "../components/projectCard/ProjectCard";
-import { projects } from "../db";
+import { Project } from "../types";
 
 export default function Projects() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  async function getProjects() {
+    const data = await getProjectsRequest();
+    setProjects(data);
+  }
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   return (
     <>
       <h1>Projects Page</h1>
