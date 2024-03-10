3922f3cdf2d8a6e1317334f47b2764d3e0bbbb35
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-03-10 13:51:55 -0400

    refactor: retrieve project from users

--- src/api/projects/index.tsx
+++ src/api/projects/index.tsx
@@ -7,13 +7,29 @@ export async function getProjectsRequest(): Promise<Project[]> {
   return (await httpClient.get(`${PROJECTS_ROUTE}/all`)).data;
 }
 
+export async function getProjectsByUserId(): Promise<Project[]> {
+  return (await httpClient.get(`${PROJECTS_ROUTE}/allByUser`)).data;
+}
+
 export async function createProject(data: CreateProjectDTO): Promise<void> {
   await httpClient.post(`${PROJECTS_ROUTE}/create`, data);
+}
+
+export async function assignUserToProjectRequest(
+  projectId: string,
+  userId: string
+): Promise<void> {
+  await httpClient.put(`${PROJECTS_ROUTE}/updateUsers/${projectId}`, {
+    userId,
+  });
 }
 
 export async function removeUserFromProjectRequest(
   userId: string,
   projectId: string
 ) {
-  await httpClient.put(`${PROJECTS_ROUTE}/${userId}/${projectId}`);
+  await httpClient.put(
+    `${PROJECTS_ROUTE}/removeUserFromProject/${userId}/${projectId}`,
+    { userId: userId, projectId: projectId }
+  );
 }
