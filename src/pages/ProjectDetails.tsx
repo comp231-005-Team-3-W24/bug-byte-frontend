786b08d1cb560f9dc46330fd3a5411979c3b29c5
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-03-10 13:52:59 -0400

    chore: update conditional to create bug report

--- src/pages/ProjectDetails.tsx
+++ src/pages/ProjectDetails.tsx
@@ -10,7 +10,7 @@ export default function ProjectDetails() {
       <h1>{project.name}</h1>
       <p>Description: {project.description}</p>
       <p>Company: {project.company}</p>
-      {user!.role == RolesEnum.administrator && (
+      {user!.role !== RolesEnum.administrator && (
         <button>
           <Link to={"/bug-report"} state={project._id}>
             Create Bug Report
