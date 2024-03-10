ad3479b8ac2a780253136026a3deb1044b16f478
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-03-10 13:52:35 -0400

    feat: implement add/remove users from project

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -1,22 +1,42 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import {
+  assignUserToProjectRequest,
   getProjectsRequest,
   removeUserFromProjectRequest,
 } from "../api/projects";
-import { Project, User } from "../types";
+import { Project, ProjectUser, User } from "../types";
 
 export function UserDetails() {
   const user: User = useLocation().state;
   const [projects, setProjects] = useState<Project[]>([]);
+  const [userProjects, setUserProjects] = useState<Project[]>([]);
 
   async function getProjects() {
     try {
       const result = await getProjectsRequest();
-      setProjects(result);
+      const userProjects = [] as Project[];
+      const unasignedProjects = [] as Project[];
+
+      result.forEach((project) => {
+        if (isUserAssignedToProject(project.users, user.id)) {
+          userProjects.push(project);
+        } else {
+          unasignedProjects.push(project);
+        }
+      });
+      setUserProjects(userProjects);
+      setProjects(unasignedProjects);
     } catch (error) {
       console.error(error);
     }
+  }
+
+  function isUserAssignedToProject(
+    projectUsers: ProjectUser[],
+    userId: string
+  ) {
+    return projectUsers.find((project) => project.user_id === userId);
   }
 
   useEffect(() => {
@@ -24,19 +44,20 @@ export function UserDetails() {
   }, []);
 
   async function removeUserFromProject(projectId: string) {
-    await removeUserFromProjectRequest(user._id, projectId);
+    await removeUserFromProjectRequest(user.id, projectId);
     await getProjects();
   }
 
-  function addUserToProject(projectId: string) {
-    alert(`User id ${user._id} added to project id ${projectId}`);
+  async function addUserToProject(projectId: string) {
+    await assignUserToProjectRequest(projectId, user.id);
+    await getProjects();
   }
 
-  function renderButton(projectId: string): JSX.Element {
-    const isUserAssigned = user.projects?.find(
-      ({ project_id }) => project_id === projectId
-    );
-    return isUserAssigned ? (
+  function renderButton(
+    projectUsers: ProjectUser[],
+    projectId: string
+  ): JSX.Element {
+    return isUserAssignedToProject(projectUsers, user.id) ? (
       <button disabled>User Already Assigned</button>
     ) : (
       <button onClick={() => addUserToProject(projectId)}>Add User</button>
@@ -52,12 +73,12 @@ export function UserDetails() {
       <div>
         <h2>Assigned Projects</h2>
         <div>
-          {user.projects!.length ? (
-            user.projects!.map((project) => (
-              <div key={project.project_id}>
-                <h3>{project.project_name}</h3>
+          {userProjects.length ? (
+            userProjects.map((project) => (
+              <div key={project._id}>
+                <h3>{project.name}</h3>
                 <button
-                  onClick={() => removeUserFromProject(project.project_id)}
+                  onClick={() => removeUserFromProject(project._id)}
                 >
                   Remove user from project
                 </button>
@@ -75,7 +96,7 @@ export function UserDetails() {
             return (
               <div key={project._id}>
                 <p>{project.name}</p>
-                {renderButton(project._id)}
+                {renderButton(project.users, project._id)}
               </div>
             );
           })
